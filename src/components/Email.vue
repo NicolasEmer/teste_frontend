64ba774fea9f83f112a3c5f08c0277ccbc04589c
Nícolas <nicolas.emer@universo.univates.br>
2025-08-05 01:53:38 -0300

    Feito o componente Email e ajustes no JobSection

---
+++ src/components/Email.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="newsletterBox">
+        <div class="iconContainer">
+            <i class="fas fa-paper-plane"></i>
+        </div>
+
+        <h2>Weekly newsletter</h2>
+        <p class="description">
+            We'll keep you updated when the best new remote jobs pop up on Himalayas.
+        </p>
+
+        <form>
+            <input class="emailInput" type="email" placeholder="Enter your email" />
+            <p class="privacy">
+                We care about your data in our privacy policy.
+            </p>
+            <button class="subscribeButton" type="submit">Subscribe</button>
+        </form>
+    </div>
+</template>
--- src/components/JobsSection.vue
+++ src/components/JobsSection.vue
@@ -1,16 +1,16 @@
 <template>
     <div class="containerJobsSection">
-        <div class="containerJobsSectionTitle">
-            <span>{{ jobs.length }} jobs</span>
-        </div>
         <div class="containerJobsSectionContent">
+            <span>{{ jobs.length }} jobs</span>
             <Jobs v-for="(job, index) in jobs" :key="index" :job="job" />
         </div>
+        <Email />
     </div>
 </template>
 
 <script setup>
 import Jobs from './Jobs.vue'
+import Email from './Email.vue'
 
 const props = defineProps({
     jobs: {
